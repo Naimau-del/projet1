d38daf9fa9b487feeecd7927a84b65eae9784645
Brochecyprien <cyp971@gmail.com>
2025-05-27 14:12:30 +0200

    Initial commit du projet Symfony

---
+++ src/Entity/Track.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrackRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TrackRepository::class)]
+class Track
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $duration = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $thumbnailURL = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tracks')]
+    #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')] 
+    private ?Release $album = null;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => false])]
+    private ?bool $favorite = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getThumbnailURL(): ?string
+    {
+        return $this->thumbnailURL;
+    }
+
+    public function setThumbnailURL(?string $thumbnailURL): static
+    {
+        $this->thumbnailURL = $thumbnailURL;
+
+        return $this;
+    }
+
+    public function getAlbum(): ?Release
+    {
+        return $this->album;
+    }
+
+    public function setAlbum(?Release $album): static
+    {
+        $this->album = $album;
+
+        return $this;
+    }
+
+    public function isFavorite(): ?bool
+    {
+        return $this->favorite;
+    }
+
+    public function setFavorite(bool $favorite): static
+    {
+        $this->favorite = $favorite;
+
+        return $this;
+    }
+}
